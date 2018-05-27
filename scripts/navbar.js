d231b0a722762b41bf764229288efbd417e1c925
Sasha <rashaafletcher@gmail.com>
2018-05-27 12:37:03 -0700

    Updates to project files

---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+var heroImg = document.querySelector('img.hero');
+var nav = document.getElementsByTagName('nav')[0];
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY > heroImg.height){
+    nav.classList.add('colored-in');
+    nav.classList.add('greyNavItem');
+  }
+    if(window.scrollY <= heroImg.height){
+    nav.classList.remove('colored-in');
+  }
+});
