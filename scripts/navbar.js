7af7ab5d690d65b3d8ab567ea5ca9ae88f69b4bd
Sasha <rashaafletcher@gmail.com>
2018-06-06 19:28:50 -0700

    Updates to responsiveness of Lupo project

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,71 +1,32 @@
 var heroImg = document.querySelector('img.hero');
 var nav = document.getElementsByTagName('nav')[0];
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY > heroImg.height){
-    nav.classList.add('colored-in');
-    nav.classList.add('greyNavItem');
-  }
-    if(window.scrollY <= heroImg.height){
-    nav.classList.remove('colored-in');
-  }
-});
+if (heroImg) {
+  window.addEventListener('scroll', function(){
+    if(window.scrollY > heroImg.height){
+      nav.classList.add('colored-in');
+      nav.classList.add('greyNavItem');
+    }
+      if(window.scrollY <= heroImg.height){
+      nav.classList.remove('colored-in');
+    }
+  });
 
-/*Burger Menu*/
-
-function initCancelIcon(x){
-  x.classList.toggle("change");
 }
 
-// if (typeof (window.Layout) == "undefined") {
-//     window.Layout = new Object();
+
+
+// var mobile = window.matchMedia('(max-width: 574px)');
+// function myFunction(mobile) {
+//   if (mobile.matches) {
+//     window.addEventListener('scroll', function(){
+//       if(window.scrollY > heroImg.height){
+//
+//       }
+//     })
+//   }
 // }
 //
-// (function (Layout) {
-//
-//     //return
-//     Layout.Init = Init;
-//
-//     function Init() {
-//         InitMainNav();
-//     }
-//
-//     function InitMainNav() {
-//         var mainNav$ = $('#siteNav .mainNav:first');
-//         var burger$ = $('#mainNavBurger');
-//         var nodeToggles$ = mainNav$.find('.toggle');
-//         burger$.on('click', function () {
-//             if ($(this).hasClass('open')) {
-//                 nodeToggles$.removeClass('open');
-//                 burger$.removeClass('open');
-//                 mainNav$.removeClass('open');
-//             } else {
-//                 burger$.addClass('open');
-//                 mainNav$.addClass('open');
-//             }
-//         });
-//         nodeToggles$.on('click', function (e) { _handleClick(e, false); });
-//         nodeToggles$.on('keydown', function (e) { _handleClick(e, true); });
-//
-//
-//         function _handleClick(e, isKeyPress) {
-//             var key = e.keyCode || e.which;
-//             if (isKeyPress && key !== 13) { return; }
-//             e.preventDefault();
-//             var this$ = $(e.currentTarget);
-//             var sibToggles$ = this$.closest('li').siblings('li').find('.toggle');
-//             var kidToggles$ = this$.siblings('ul').find('.toggle');
-//             if (this$.hasClass('open')) {
-//                 sibToggles$.removeClass('open');
-//                 kidToggles$.removeClass('open');
-//                 this$.removeClass('open');
-//             } else {
-//                 sibToggles$.not(this$).removeClass('open');
-//                 kidToggles$.removeClass('open')
-//                 this$.addClass('open');
-//             }
-//         }
-//     }
-//
-//
-// });
+// window.addEventListener('scroll', function(){
+//   if(responsive.matches)
+// })
