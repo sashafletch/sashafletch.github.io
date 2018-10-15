ff24decff0e396ae006ce0bfa3553e99576840ea
Sasha <rashaafletcher@gmail.com>
2018-10-14 19:20:57 -0700

    Project redesign

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,16 +6,18 @@ $(document).ready(function(){
     scroll_start = $(this).scrollTop();
     if(scroll_start > offset.top) {
       $('#navbar').css('background-color', '#ffffff');
-      $('.dropdown-content').css('background-color', '#ffffff');
+      $('.large-nav-item').css('background-color', '#ffffff');
       $('.nav_main li').css('color', '#000000');
       $('#nav_about a').css('color', '#000000');
       $('#nav_resume a').css('color', '#000000');
+      $('#navbar').css('box-shadow', '0 2px 2px -2px rgba(0,0,0,.2)');
     } else {
       $('#navbar').css('background-color', 'transparent');
-      $('.dropdown-content').css('background-color', 'rgba(250, 250, 250, 0.7)');
+      $('.large-nav-item').css('background-color', 'rgba(250, 250, 250, 0.7)');
       $('.nav_main li').css('color', '#ffffff');
       $('#nav_about a').css('color', '#ffffff');
       $('#nav_resume a').css('color', '#ffffff');
+      $('#navbar').css('box-shadow', 'none');
     }
   });
 });
